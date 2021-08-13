14af06334fac536fec324d7ca0fb859388ba73d3
Ramsey Gavin <ramseygavin@Ramsey-Gavin.local>
2021-08-12 20:34:28 -0700

    checkpoint

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const app = require('express')();
+/**
+ * Shows where to hit the endpoint in
+ * the client localhost:8080
+ */
 const PORT = 8080;
 
-app.listen(PORT, () => console.log('Its alive again'))
+app.listen(PORT, () => console.log('Its alive again 👾', __dirname))
 
 app.get('/ramsey', (req, res) => {
     res.status(200).send({
         firstname: "Ramsey",
         lastname: "Gavin"
     })
+})
+
+app.get('/brooke', (req, res) => {
+    res.status(500).send('WTF 🤯')
 })
